8aa755d4bbc3894f91559529c5dc43667e7e3ec6
spayker <spykerstar@gmail.com>
2020-09-30 00:15:48 +0200

    SBM-11 updated vector icons on main screen

--- src/components/common/navigator/appNavigator.js
+++ src/components/common/navigator/appNavigator.js
@@ -9,12 +9,12 @@ import MainMenu from '../../mainMenu/mainMenu'
  * where 'this' - current component/screen from where navigation will go next
  */
 const AppNavigator = createStackNavigator({
-  SignUpEmail: {
-    screen: SignUpEmail,
-    navigationOptions: { header: null }
-  },
   MainMenu: {
     screen: MainMenu,
+    navigationOptions: { header: null }
+  },
+  SignUpEmail: {
+    screen: SignUpEmail,
     navigationOptions: { header: null }
   },
   SignInEmail: {
