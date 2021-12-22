480bf83db5d954a2cf70be9e74b30a870e90b5ef
spayker <spykerstar@gmail.com>
2021-12-22 23:08:22 +0100

    added skeleton of main screen

--- app/src/androidTest/java/com/spikeboost/activity/auth/SignInActivityTest.java
+++ app/src/androidTest/java/com/spikeboost/activity/auth/SignInActivityTest.java
@@ -3,8 +3,13 @@ package com.spikeboost.activity.auth;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class SignInActivityTest {
@@ -14,8 +19,9 @@ public class SignInActivityTest {
             new ActivityScenarioRule<>(SignInActivity.class);
 
     @Test
-    public void click_Back_Button_Shows_LoginActivity(){
-
+    public void click_Back_Button_Performs_Exit(){
+        Espresso.pressBackUnconditionally();
+        assertEquals(Lifecycle.State.CREATED, loginActivityRule.getScenario().getState());
     }
 
 
